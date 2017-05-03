03ef3e3d2aac703102fc1e54412b37b22676a50c
星野斉 <hhoshino-main@memomad.jp>
2017-05-03 13:23:29 +0900

    新規コミット処理

---
+++ FreiendlyMapReduce/cmake-build-debug/CMakeFiles/FreiendlyMapReduce.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FreiendlyMapReduce.dir/main.cpp.o"
+  "FreiendlyMapReduce.pdb"
+  "FreiendlyMapReduce"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FreiendlyMapReduce.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
